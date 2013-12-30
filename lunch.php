8ac3c0de16b8afd610217ebd2e2df250efe2be68
Ryan Duff <ryan@fusionized.com>
2013-12-30 18:30:15 -0500

    Updated lunch choices to an array
    Plucks random choice out of array.
    Removed "mexican" choice due to indigestion.

--- lunch.php
+++ lunch.php
@@ -1,15 +1,16 @@
 <?php
 
-// Defines lunch ("LUNCH")
-
 if ( wp_checkdate( 12, 31, 2013 ) ) {
 
-LUNCH == __('chinese');
-LUNCH == __('mexican');
-LUNCH == __('italian');
-LUNCH == __('subs');
-LUNCH == __('starbucks');
-LUNCH == __('some-sorta-sandwich');
-LUNCH == __('caffeine');
+$choices = array(
+    __('chinese'),
+    __('italian'),
+    __('subs'),
+    __('starbucks'),
+    __('some-sorta-sandwich'),
+    __('caffeine'),
+);
+
+$lunch = $choices[array_rand( $choices )];
 
 }
